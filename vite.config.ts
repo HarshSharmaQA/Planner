3a922f012b1e41a87ede15e5a233ee6b12a97df1
Harsh Sharma <harshsharmaqa@gmail.com>
2025-08-20 20:00:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Planner//', // Update this to your repository name
+  base: '/Planner/', // 👈 must match repo name exactly
   build: {
     outDir: 'dist',
     sourcemap: true,
